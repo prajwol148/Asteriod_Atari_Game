d38999eb1384e7fa3682cd9e5e8fe85b26d58d93
prajw <prajwoltiwari2000@gmail.com>
2024-05-01 15:02:38 -0500

    Changed the rotation functionality by implementing HashMap and recording the keyCode. This reduced the system delay to respond to continuos pressing of same key. The hashmap records the keyCode and its value as Booleamn value; if the key is being pressed then the value of set True and the moment the key is released, the boolean value is set to False. The AnimationTimer class was used t continuously update the value.

--- src/main/java/ssj/project_game/AsteroidApplication.java
+++ src/main/java/ssj/project_game/AsteroidApplication.java
@@ -1,18 +1,55 @@
 package ssj.project_game;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Polygon;
 import javafx.stage.Stage;
+
+import java.util.HashMap;
 
 public class AsteroidApplication extends Application {
     public void start(Stage stage) throws Exception{
         Pane pane= new Pane();
         pane.setPrefSize(600,400);
 
+        //Creating ship
+        Polygon triangleShip = new Polygon(-5, -5, 10, 0, -5, 5);
+        triangleShip.setTranslateX(300);
+        triangleShip.setTranslateY(200);
+
+
+
+        pane.getChildren().add(triangleShip);
+
 
         Scene scene= new Scene(pane);
+
+        HashMap<KeyCode, Boolean> keypressed= new HashMap<>();
+        scene.setOnKeyPressed(event->{
+                keypressed.put(event.getCode(), Boolean.TRUE);
+
+        });
+        scene.setOnKeyReleased(event->{
+            keypressed.put(event.getCode(), Boolean.FALSE);
+        });
+
+        new AnimationTimer(){
+            public void handle(long now){
+                if(keypressed.getOrDefault(KeyCode.LEFT, false)){
+                    triangleShip.setRotate(triangleShip.getRotate()-5);
+                }
+                if(keypressed.getOrDefault(KeyCode.RIGHT,false)){
+                    triangleShip.setRotate(triangleShip.getRotate()+5);
+                }
+            }
+        }.start();
+
+
+
         stage.setTitle("Asteriod- Atari Game");
         stage.setScene(scene);
         stage.show();
