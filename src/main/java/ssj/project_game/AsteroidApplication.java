8f040e11bb7d080fb5bcd0d5a8ca8e1a860fccd1
prajw <prajwoltiwari2000@gmail.com>
2024-05-01 13:45:15 -0500

    Stage created and title set.

--- src/main/java/ssj/project_game/AsteroidApplication.java
+++ src/main/java/ssj/project_game/AsteroidApplication.java
@@ -1,2 +1,20 @@
-package ssj.project_game;public class AsteroidApplication {
+package ssj.project_game;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
+public class AsteroidApplication extends Application {
+    public void start(Stage stage) throws Exception{
+        Pane pane= new Pane();
+        pane.setPrefSize(600,400);
+
+
+        Scene scene= new Scene(pane);
+        stage.setTitle("Asteriod- Atari Game");
+        stage.setScene(scene);
+        stage.show();
+    }
 }
